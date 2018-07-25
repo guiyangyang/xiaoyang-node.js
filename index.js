ef45725de6a12218719e8f07ba988e894b706fed
guiyangyang <guiyangyang@playhudong.com>
2018-07-25 14:54:49 +0800

    node.js 起步

---
+++ index.js
@@ -0,0 +1,4 @@
+//index.js 中使用自定义的模块
+const greeting = require('./src/greeting');
+
+greeting.hello();
---
+++ src/greeting.js
@@ -0,0 +1,6 @@
+//新建 src/greeting.js   greeting.js中自定义模块 代码如下
+const hello = ()=>{
+  console.log('hellod~')
+}
+
+module.exports.hello = hello
